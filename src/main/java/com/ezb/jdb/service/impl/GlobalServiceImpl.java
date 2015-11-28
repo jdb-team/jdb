9a49d46a5500c4d31e5674a433f9344bf9c415d9
wuyw <839427538@qq.com>
2015-11-28 10:04:05 +0800

    活动按pv排序

--- src/main/java/com/ezb/jdb/service/impl/GlobalServiceImpl.java
+++ src/main/java/com/ezb/jdb/service/impl/GlobalServiceImpl.java
@@ -45,7 +45,7 @@ public class GlobalServiceImpl implements IGlobalService {
             PageResult<Friend> friendPageResult, String keyword) {
 
         circlePageResult = circleDao.queryCircles(circlePageResult, keyword);
-        activityPageResult = activityDao.queryActivity(activityPageResult, keyword,"");
+        activityPageResult = activityDao.queryActivity(activityPageResult, keyword,"","2");
         topicPageResult = topicDao.queryTopic(topicPageResult, keyword);
         friendPageResult = friendDao.queryFriend(friendPageResult, phone, keyword, 1);
 
